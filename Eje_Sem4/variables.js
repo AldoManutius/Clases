22692551b7b4e3b1396f339eecbd256c2323e67a
AldoManutius <amartin_lopez@hotmail.com>
2022-02-15 20:19:39 +0100

    15_2

---
+++ Eje_Sem4/variables.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const nombre = "Alex"
+
+const apellido = "Martin"
+
+let edad = 38
+
+const color = "azul"
+
+console.log(typeof 38);
+
+console.log(`Hola, me llamo ${nombre} ${apellido}, tengo ${edad} años y mi color favorito es el ${color}`);
